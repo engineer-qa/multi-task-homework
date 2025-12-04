03658d7f2216b713b4e1f9b6117b89489ae57a8a
Vyacheslav <vyacheslav.popov.qa@gmail.com>
2025-12-04 16:04:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿int barberCapacity = 1;
+int waitingChairsCapacity = 3;
+int totalClients = 10;
+
+Semaphore barber = new Semaphore(barberCapacity, barberCapacity);
+Semaphore waitingChairs = new Semaphore(waitingChairsCapacity, waitingChairsCapacity);
+
+for (int i = 1; i < totalClients; i++)
+{
+    int id = i;
+    new Thread(() => GetHaircut(id)).Start();
+}
+
+void GetHaircut(int clientId)
+{
+    Console.WriteLine($"Клиент {clientId} заходит в барбершоп");
+
+    if (!waitingChairs.WaitOne(0))
+    {
+        Console.WriteLine($"Клиент {clientId} уходит, так как нет свободных мест");
+        return;
+    }
+    Console.WriteLine($"Клиент {clientId} садится в зал ожидания");
+
+    barber.WaitOne();
+    waitingChairs.Release();
+
+    Console.WriteLine($"Клиент {clientId} стрижется");
+    Thread.Sleep(new Random().Next(1000, 3000));
+    Console.WriteLine($"Клиент {clientId} выходит из барбершопа");
+
+    barber.Release();
+}
